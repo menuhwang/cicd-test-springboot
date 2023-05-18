3cecccec5c6e5263f9262affa34d68b6d0fa56f4
menuHwang <hmin0206@naver.com>
2023-05-18 23:37:57 +0900

    테스트 엔드포인트 추가

--- src/main/java/com/example/cicdtest/HelloController.java
+++ src/main/java/com/example/cicdtest/HelloController.java
@@ -1,15 +1,23 @@
 package com.example.cicdtest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/hello")
 public class HelloController {
     private static final String MSG = "Hello";
-    @GetMapping
+
+    @Value("${hello.msg}")
+    private String msg;
+
+    @GetMapping("/hello")
     public String hello() {
         return MSG;
+    }
+
+    @GetMapping("/hi")
+    public String hi() {
+        return msg;
     }
 }
